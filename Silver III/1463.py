271987c6241118c323d9aac298ab9869956ea8eb
YoungJae Kim <corea_kyj@naver.com>
2022-03-01 21:41:20 +0900

    solved 2022.03.01

---
+++ Silver III/1463.py
@@ -0,0 +1,27 @@
+'''
+문제: 1로 만들기(https://www.acmicpc.net/problem/1463)
+분류: 다이나믹 프로그래밍
+시간 복잡도: O(n)
+'''
+from typing import List
+
+
+def solution(n: int) -> int:
+    dp: List[int] = [0] * (n + 1)
+    dp[1] = 0
+    for i in range(2, n + 1):
+        dp[i] = dp[i-1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+    return dp[n]
+
+
+def main():
+    n: int = int(input())
+    print(solution(n))
+
+
+if __name__ == '__main__':
+    main()
